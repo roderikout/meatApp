be552005b25544935489b18d8580c8554f24de37
Roderikout <roderikout@gmail.com>
2019-03-31 14:58:13 +0200

    con helper service cambio insignificante

--- meatappv1/src/app/home/home.module.ts
+++ meatappv1/src/app/home/home.module.ts
@@ -15,14 +15,6 @@ import { HomePage } from './home.page';
       {
         path: '',
         component: HomePage,
-        /* children: [
-          { path: 'cut-list', loadChildren: '../pages/cut-list/cut-list.module#CutListPageModule' },
-          { path: 'cut-add', loadChildren: '../pages/cut-add/cut-add.module#CutAddPageModule' },
-          { path: 'recipe-list', loadChildren: '../pages/recipe-list/recipe-list.module#RecipeListPageModule' },
-          { path: 'recipe-add', loadChildren: '../pages/recipe-add/recipe-add.module#RecipeAddPageModule' },
-          { path: 'cut-detail/:id', loadChildren: '../pages/cut-detail/cut-detail.module#CutDetailPageModule' },
-          { path: 'recipe-detail/:id', loadChildren: '../pages/recipe-detail/recipe-detail.module#RecipeDetailPageModule'},
-        ] */
       }
     ])
   ],
