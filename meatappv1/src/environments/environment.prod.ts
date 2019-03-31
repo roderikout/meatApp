5b187776b457570b0ed8d4a95b947008a46ffb1f
Roderikout <roderikout@gmail.com>
2019-03-31 22:46:49 +0200

    Con carpetas de Cordova

--- meatappv1/src/environments/environment.prod.ts
+++ meatappv1/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCw0oc-p0_IQSCKvlseo6r55sw49HDyrCQ',
+    authDomain: 'meatappv1.firebaseapp.com',
+    databaseURL: 'https://meatappv1.firebaseio.com',
+    projectId: 'meatappv1',
+    storageBucket: 'meatappv1.appspot.com',
+    messagingSenderId: '214502559450'
+  }
 };
